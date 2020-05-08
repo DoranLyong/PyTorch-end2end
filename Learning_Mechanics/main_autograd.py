827bae99dc6d959a0c1672d9d8db260f2a38d000
DoranLyong <cheeryun@gmail.com>
2020-05-08 20:01:09 +0900

    Learning_Mechanics

--- Learning_Mechanics/main_autograd.py
+++ Learning_Mechanics/main_autograd.py
@@ -1,30 +1,30 @@
 import torch
 
 
-
-
+# _Start: define your model
 def model(t_u:torch.Tensor, w:torch.Tensor, b:torch.Tensor):
     return w * t_u + b
+# _End: define your model
 
+
+# _Start: define the loss function
 def loss_fn(t_p:torch.Tensor, t_c:torch.Tensor) -> torch.Tensor:
     squared_diff = (t_p - t_c)**2
     return squared_diff.mean()
+# _End: define the loss function
 
 
 # _Start: define training loop with autograd
 def training_loop(n_epochs, learning_rate, params, t_u, t_c):
     for epoch in range(1, n_epochs + 1):
-
-        # _Start: set zero init grad
-        if params.grad is not None:
+        if params.grad is not None:  # <1>
             params.grad.zero_()
-        # _End: set zero init grad
 
         t_p = model(t_u, *params)
         loss = loss_fn(t_p, t_c)
-        loss.backward()  # backpropagation
+        loss.backward()
 
-        with torch.no_grad():
+        with torch.no_grad():  # <2>
             params -= learning_rate * params.grad
 
         if epoch % 500 == 0:
@@ -34,13 +34,13 @@ def training_loop(n_epochs, learning_rate, params, t_u, t_c):
 # _End: define training loop with autograd
 
 
-
 def main():
     # _Start: create data
     t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]     # currect
     t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4] # input
     t_c = torch.tensor(t_c)
     t_u = torch.tensor(t_u)
+    t_un = 0.1 * t_u
     # _End: Create data
 
 
@@ -60,11 +60,14 @@ def main():
 
     # _Start: weight updates with gradient
     training_loop(
-        n_epochs=100, learning_rate=1e-4,
-        params=torch.tensor([1.0, 0.0], requires_grad=True) ,
-        t_u=t_u, t_c=t_c )
+        n_epochs=5000,
+        learning_rate=1e-2,
+        params=torch.tensor([1.0, 0.0], requires_grad=True),  # <1>
+        t_u=t_un,  # <2>
+        t_c=t_c)
     # _End: weight updates with gradient
 
+    print("End...")
 
 
 if __name__ == "__main__":
